2ca7b6fe297d357d66bb4e056c85d2ec1d2f9773
Wang-yin <sky06456@gmail.com>
2025-04-13 00:47:22 +0800

    feat: interactive-comments-section

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = 'https://interactive-comments-section-wua8.onrender.com';
 
 export const getData = async () => {
   try {
