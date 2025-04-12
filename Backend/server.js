4d7e42c650d0119599249a54ef5c2435f17e6ab3
Wang-yin <sky06456@gmail.com>
2025-04-12 23:41:33 +0800

    fix: update CORS configuration for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -6,13 +6,18 @@ const comments = require("./routes/comment")
 const cors = require('cors')
 const corsOptions = {
   origin: ["https://yininteractive-comments-section.netlify.app"],
+  methods: ["GET", "POST", "PATCH", "DELETE"],
 }
 
-
+app.use(cors(corsOptions))
 app.use(express.json());
 
-app.use(cors(corsOptions))
+
 app.use('/', comments)
+
+app.get("/", (req, res) => {
+  res.send("API running!");
+});
 
 
 
